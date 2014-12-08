2cbc2bac376f706cf277a3abe9e72e2459caeb12
Patryk Wąsiewicz <patryk.wasiewicz@nau.pl>
2014-12-08 15:10:50 +0100

    Missing files from previous commit.

---
+++ MiniAutFac/ResolvableExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace MiniAutFac
+{
+    using System;
+    using Interfaces;
+
+    public static class ResolvableExtensions
+    {
+        /// <summary>
+        /// Resolves the specified resolvable.
+        /// </summary>
+        /// <typeparam name="T">The typ to resolve.</typeparam>
+        /// <param name="resolvable">The resolvable.</param>
+        /// <returns>Resolved instance.</returns>
+        /// <exception cref="System.ArgumentNullException">resolvable</exception>
+        public static T Resolve<T>(this IResolvable resolvable)
+        {
+            if (resolvable == null)
+            {
+                throw new ArgumentNullException("resolvable");
+            }
+
+            return (T)resolvable.Resolve(typeof(T));
+        }
+    }
+}
---
+++ MiniAutFac/ScopeExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace MiniAutFac
+{
+    using Scopes.DefaultScopes;
+
+    public static class ScopeExtensions
+    {
+        /// <summary>
+        /// Pers the dependency.
+        /// </summary>
+        /// <param name="resolvable">The resolvable.</param>
+        /// <returns></returns>
+        public static BuilderResolvableItemBase PerDependency(this BuilderResolvableItemBase resolvable)
+        {
+            resolvable.Scope = new PerDependencyScope();
+            return resolvable;
+        }
+
+        /// <summary>
+        /// Pers the lifetime scope.
+        /// </summary>
+        /// <param name="resolvable">The resolvable.</param>
+        /// <returns></returns>
+        public static BuilderResolvableItemBase PerLifetimeScope(this BuilderResolvableItemBase resolvable)
+        {
+            resolvable.Scope = new PerLifetimeScope();
+            return resolvable;
+        }
+
+        /// <summary>
+        /// Singles the instance.
+        /// </summary>
+        /// <param name="resolvable">The resolvable.</param>
+        /// <returns></returns>
+        public static BuilderResolvableItemBase SingleInstance(this BuilderResolvableItemBase resolvable)
+        {
+            resolvable.Scope = new SingleInstanceScope();
+            return resolvable;
+        }
+    }
+}
