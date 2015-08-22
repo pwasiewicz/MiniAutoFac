ee7537780256f5c91a73e60526d74be4f7893f4a
Patryk Wąsiewicz <pat.wasiewicz@gmail.com>
2015-08-22 16:57:19 +0200

    Scope impl

---
+++ MiniAutFac.ScopePerRequest/PerRequestScope.cs
@@ -0,0 +1,56 @@
+﻿namespace MiniAutFac.ScopePerRequest
+{
+    using Exceptions;
+    using Scopes;
+    using System;
+    using System.Collections.Generic;
+    using System.Web;
+
+    public static class PerReqestScopeHelper
+    {
+        public static ItemRegistrationBase PerRequest(this ItemRegistrationBase resolvable)
+        {
+            return resolvable.WithScope(new PerRequestScope());
+        }
+    }
+
+    public class PerRequestScope : Scope
+    {
+        private readonly IDictionary<HttpContext,Dictionary<Type, object>> requestMap;
+
+        public PerRequestScope()
+        {
+            this.requestMap = new Dictionary<HttpContext, Dictionary<Type, object>>();
+        }
+
+        public override void GetInstance(LifetimeScope lifetimeScope, Func<object> valueFactory, Type valueType,
+                                         out object value)
+        {
+            var request = HttpContext.Current;
+            if (request == null) throw new CannotResolveTypeException();
+
+            lock (request)
+            {
+                if (!this.requestMap.ContainsKey(request))
+                {
+                    this.requestMap.Add(request, new Dictionary<Type, object>());
+                    request.AddOnRequestCompleted(ctx => this.requestMap.Remove(ctx));
+                }
+
+                Dictionary<Type, object> scopeTypes;
+
+                if (!this.requestMap.TryGetValue(request, out scopeTypes))
+                {
+                    scopeTypes = new Dictionary<Type, object>();
+                    this.requestMap.Add(request, scopeTypes);
+                }
+
+                if (!scopeTypes.TryGetValue(valueType, out value))
+                {
+                    value = valueFactory();
+                    scopeTypes.Add(valueType, value);
+                }
+            }
+        }
+    }
+}
